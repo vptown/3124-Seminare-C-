b3ca26d2e832a9b16a49242d3983cd5687d84d65
Sultanov Rustam <v.p.town@gmail.com>
2022-09-03 15:25:52 +0500

    Задача 0

--- seminar1/tast0/Program.cs
+++ seminar1/tast0/Program.cs
@@ -1,20 +1,11 @@
-﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
-// a = 25, b = 5 -> да 
-// a = 2, b = 10 -> нет 
-// a = 9, b = -3 -> да 
-// a = -3 b = 9 -> нет
+﻿// Напишите программу, которая на вход принимает число и выдаёт его 
+// квадрат (число умноженное на само себя).
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
 
-Console.WriteLine("Введите число 1:");
-int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число 2:");
-int numberB = Convert.ToInt32(Console.ReadLine());
-
-if(numberA == numberB * numberB)
-{
-    Console.WriteLine(" Да ");
-} 
-else 
-{
-    Console.WriteLine(" Нет ");
-}
+Console.WriteLine(number * number);
