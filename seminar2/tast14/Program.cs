dc8eefaff6f90588267c2fb3df0cb02c26a1f42f
Sultanov Rustam <v.p.town@gmail.com>
2022-09-07 00:59:48 +0500

    Задача 14

---
+++ seminar2/tast14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 14: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число :");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int digit1 = 7;
+int digit2 = 23;
+if(n % digit1 == 0 && n % digit2 == 0)
+{
+    Console.WriteLine("ДА"); 
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
