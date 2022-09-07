98e337b0a54f740cb483ef67ebae32201da06717
Sultanov Rustam <v.p.town@gmail.com>
2022-09-07 22:20:26 +0500

    Задача 16

--- seminar2/tast16/Program.cs
+++ seminar2/tast16/Program.cs
@@ -10,14 +10,14 @@ int n1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2:");
 int n2 = Convert.ToInt32(Console.ReadLine());
 
-if(n1 == n2 * n2)
+if(n1 == n2 * n2 || n2 == n1 * n1 )
 {
     Console.WriteLine("Да");
 }
- else if(n2 == n1 * n1)
- {
-     Console.WriteLine("Да");
- }
+//  else if(n2 == n1 * n1)
+//  {
+//      Console.WriteLine("Да");
+//  }
 else
 {
     Console.WriteLine("Нет");
