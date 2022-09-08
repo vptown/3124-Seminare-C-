83029fafaed1690067b49b50c810c20b6478d5a0
Sultanov Rustam <v.p.town@gmail.com>
2022-09-09 00:30:29 +0500

    Задача 20

---
+++ seminar3/tast20/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 20: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+// Формула вычисления расстояния между двумя точками A(xa, ya) и B(xb, yb) на плоскости:
+// AB = √(xb - xa)2 + (yb - ya)2
+// number = Math.Pow(number,2) вщзведение в степень.
+// number = Math.Sqrt(number) корень.
+
+Console.WriteLine("Введите координату точки A по X:");
+int xa = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату точки A по Y:");
+int ya = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату точки B по X:");
+int xb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату точки B по Y:");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+double res = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2)) ;
+Console.WriteLine($"{res:f4}");
