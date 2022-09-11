a964288afc8f6430adb670ee2a9af1ac92eb679b
Sultanov Rustam <v.p.town@gmail.com>
2022-09-11 13:53:43 +0500

    Домашняя задача 23

---
+++ seminar3/hz23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.Write( i * i * i + " ");
+    }
+}
+else
+{
+    Console.WriteLine( "i должно быть больше 1");
+}
