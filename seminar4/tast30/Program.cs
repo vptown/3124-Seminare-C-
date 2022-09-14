3b695dfac19149da45bc5fc6e2a17b193f9c66bf
Sultanov Rustam <v.p.town@gmail.com>
2022-09-14 20:42:44 +0500

    Задача 30

---
+++ seminar4/tast30/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+Random rand = new Random();
+for (int n = 0; n < array.Length; n++)
+   array[n] = rand.Next(0,2);
+
+   foreach (int arr in array)
+   {
+    Console.Write(arr + " ");
+   }
