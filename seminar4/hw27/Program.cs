aa62721987161b64d7fdef43adf65178b74b77fb
Sultanov Rustam <v.p.town@gmail.com>
2022-09-16 20:53:57 +0500

    Домашняя задача 27

---
+++ seminar4/hw27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("Сумма всех цифр в числе равна: " + sum);
+
