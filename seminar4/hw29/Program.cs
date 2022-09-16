26dc6d34cc94d3361e69e8f3c518322c0a642c55
Sultanov Rustam <v.p.town@gmail.com>
2022-09-16 21:41:29 +0500

    Домашняя задача 29

---
+++ seminar4/hw29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] array = new int[lenArray];
+for (int i = 0; i < array.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
