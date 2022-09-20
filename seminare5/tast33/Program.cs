ef92e7a965d772abf0e6b3770e1fe0f253f6dc36
Sultanov Rustam <v.p.town@gmail.com>
2022-09-20 21:04:23 +0500

    Задача 37

--- seminare5/tast33/Program.cs
+++ seminare5/tast33/Program.cs
@@ -4,7 +4,20 @@
 
 
 
-int[] GetArray(int size,int leftRange,  int rightRange)
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(string.Join(", ", array));
+int find = ReadNumber("Задайте число, которое хотите найти: ");
+bool res = IsNumberInArray(array, find);
+if(res == true)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
+int[] GetArray(int size, int leftRange, int rightRange)
 {
     int[] arr = new int[size];
     Random rand = new Random();
@@ -13,4 +26,25 @@ int[] GetArray(int size,int leftRange,  int rightRange)
         arr[i] = rand.Next(leftRange, rightRange + 1);
     }
     return arr;
+}
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+//true - есть элемент в массиве
+//false - элемент в массиве отсутствует
+bool IsNumberInArray(int[] arr, int findNumber)
+{
+    bool isNumberInArray = false; //
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == findNumber)
+        {
+            isNumberInArray = true;
+        }
+    }
+    return isNumberInArray;
 }
---
+++ seminare5/tast37/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+// Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] arr = { 1, 2, 3, 4, 5 };
+int[] arrnew = new int[(arr.Length + 1) / 2];
+
+for (int i = 0; i < arrnew.Length; i++)
+{
+    arrnew[i] = arr[i] * arr[arr.Length - 1 - i];
+    if (i == arr.Length - 1 - i)
+    {
+        arrnew[i] = arr[i];
+    }
+}
+
+Console.WriteLine(string.Join(", ", arrnew));
