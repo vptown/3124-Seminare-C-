edc2f8f1d5ed6a21ecd50a7216442ee0c676d3e3
Sultanov Rustam <v.p.town@gmail.com>
2022-09-20 21:02:22 +0500

    Задача 33

--- seminare5/tast32/Program.cs
+++ seminare5/tast32/Program.cs
@@ -28,3 +28,29 @@ int[] GetArray(int size,int leftRange,  int rightRange)
  //NumNeg(array);
  Console.WriteLine(string.Join(", ", newArray));
                                  // array
+
+
+//              int[] GetArray(int size, int leftRange, int rightRange)
+// {
+    
+//     int[] arr = new int[size];
+//     Random rand = new Random();
+//     for(int i = 0; i < size; i++)
+//     {
+//         arr[i] = rand.Next(leftRange, rightRange + 1);
+//     }
+//     return arr;
+// }
+
+// void SwapSigns(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = -array[i];
+//     }
+// }
+
+// int[] array = GetArray(12, -9, 9);
+// Console.WriteLine(string.Join(", ", array));
+// SwapSigns(array);
+// Console.WriteLine(string.Join(", ", array));                    
---
+++ seminare5/tast33/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+
+
+int[] GetArray(int size,int leftRange,  int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
