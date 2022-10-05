1de7e4c746b3bcdf0115fb525787de8532c638ad
Sultanov Rustam <v.p.town@gmail.com>
2022-10-05 19:36:09 +0500

    Домашняя задача 35

--- seminare5/hw35/Program.cs
+++ seminare5/hw35/Program.cs
@@ -1,22 +1,37 @@
-﻿//Задача 35: Задайте одномерный массив из 123 случайных чисел в диапазоне [0, 150]. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+﻿//Задача 35: Задайте одномерный массив из 123 случайных чисел в диапазоне [0, 150]. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
 // Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
 // [5, 18, 123, 6, 2] -> 1
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
 
-int N=123;
-int[] arr = new int[N];
-Random random = new Random();
-for(int i = 0; i < arr.Length; i++)
-arr[i] = random.Next(0,10);
-for(int i = 0;i < arr.Length; i++)
-Console.Write($"{arr[i],4}");
 
-int count = 0;
-for(int i = 0; i < arr.Length; i++)
+
+int[] GetArray(int size,int leftRange,  int rightRange)
 {
-if( i > 10 && i < 99)
-count++;
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+int Pos (int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if( i > 10 && i < 99) 
+        {
+            sum ++; 
+        }
+    }
+    return sum;
 }
 
-Console.WriteLine(count);
+int [] array = GetArray( 123, 0, 151);
+ Console.WriteLine(string.Join(", ", array));
+int newArray =  Pos(array);
+Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10,99] равна {newArray}");
+
