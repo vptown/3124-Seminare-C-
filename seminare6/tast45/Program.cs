3f5a4acfa6f57683729999d50315e684132cf12f
Sultanov Rustam <v.p.town@gmail.com>
2022-10-07 19:26:12 +0500

    tast45

---
+++ seminare6/tast45/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного копирования.
+int[] GetArray(int size, int leftRange,  int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    size = 16;
+    return arr;
+    
+}
+
+int[] arr = GetArray(5, 0, 10);
+int[] copiedArray = new int[arr.Length];
+
+for(int i = 0; i < arr.Length; i++)
+{
+    copiedArray[i] = arr[i];
+}
+arr[0] = 100;
+copiedArray[1] = 100;
+Console.WriteLine($"Исходный массив = {string.Join(", ", arr)}");
+Console.WriteLine($"Скопированный массив = {string.Join(", ", copiedArray)}");
