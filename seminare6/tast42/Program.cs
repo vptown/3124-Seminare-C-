4462dcca8fec99b64e433295f5a4dddc1e52e800
Sultanov Rustam <v.p.town@gmail.com>
2022-10-07 15:57:31 +0500

    tast42

---
+++ seminare6/tast42/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintBinaryView(int number)
+{
+    if(number <= 0) return;
+    PrintBinaryView(number / 2);
+    Console.Write(number % 2);
+}
+
+int num = ReadNumber("Введите число:");
+PrintBinaryView(num);
+
+
