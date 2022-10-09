6412360a08fb0604fd24ec056fdda9b9f7cff444
Sultanov Rustam <v.p.town@gmail.com>
2022-10-09 20:43:55 +0500

    hw41

--- seminare6/hw41/Program.cs
+++ seminare6/hw41/Program.cs
@@ -1,17 +1,25 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. Ввод чисел останавливается при помощи ввода стоп-слова "stop"
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3int ReadNumber(string message)
-// int ReadNumber(string message)
-// {
-//     Console.WriteLine(message);
-//     return Convert.ToInt32(Console.ReadLine());
-// }
+Console.WriteLine("Введите число: ");
+int count = 0;
+while (true)
+{
 
+    string s = Console.ReadLine();
+    if (s == "stop") break;
+    
 
-//  int num = ReadNumber("Введите число:");
-//  int number = 0;
-//while(true)Console.ReadLine();
+    else
+    {
+        int x = Convert.ToInt32(s);
 
- 
-   
+        if (x > 0)
+        {
+            count++;
+        }
+    }
+}
+Console.Write($"Количество положительных чисел равно {count}");
+
 
