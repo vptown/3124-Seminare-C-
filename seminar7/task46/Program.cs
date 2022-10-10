b06c6215e905f3ac38186d296f099999e83fbc19
Sultanov Rustam <v.p.town@gmail.com>
2022-10-10 22:20:09 +0500

    tast46

---
+++ seminar7/task46/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+ 
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = -10, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+   for(int i = 0;  i < matrix.GetLength(0); i++ )
+   {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rand.Next(leftRange, rightRange + 1);
+        }
+   }
+   return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for(int i = 0;  i < matrix.GetLength(0); i++ )
+   {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+           Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+   }
+}
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
