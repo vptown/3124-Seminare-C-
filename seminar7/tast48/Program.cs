a53772c9e69f26b3bfd440b28954fbb827e7f4bd
Sultanov Rustam <v.p.town@gmail.com>
2022-10-11 14:09:45 +0500

    tast48

---
+++ seminar7/tast48/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле:
+// Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+ 
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+   
+   
+   for(int i = 0;  i < matrix.GetLength(0); i++ )
+   {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = i + j;
+        }
+
+   }
+   return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for(int i = 0;  i < matrix.GetLength(0); i++ )
+   {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+           Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+   }
+}
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
---
+++ seminar7/tast51/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+ 
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = -10, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+   for(int i = 0;  i < matrix.GetLength(0); i++ )
+   {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rand.Next(leftRange, rightRange + 1);
+        }
+   }
+   return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for(int i = 0;  i < matrix.GetLength(0); i++ )
+   {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+           Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+   }
+}
+
+void PosMatrix(int[,] matrix)
+{
+for(int i = 0;  i < matrix.GetLength(0); i+= 2)
+   {
+        for(int j = 0; j < matrix.GetLength(1); j+= 2)
+        {
+           matrix[i,j] = matrix[i,j] * matrix[i,j];
+          
+        }
+
+        Console.WriteLine();
+        
+   }
+   
+}
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
+PosMatrix(matr);
+PrintMatrix(matr);
