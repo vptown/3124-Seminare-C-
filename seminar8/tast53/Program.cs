8850fb41c0148bdf729f7d402fedc7d4e21da37b
Sultanov Rustam <v.p.town@gmail.com>
2022-10-13 00:09:25 +0500

    tast55

---
+++ seminar8/tast53/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ReverseArray(int[,] matrix)
+{
+    int temp;
+    
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           
+            
+                temp = matrix[0, j];
+                matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+                matrix[matrix.GetLength(0) - 1, j] = temp;
+            
+        }
+        Console.WriteLine();
+    }
+    
+
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+int[,] matrix = GetMatrix(m, n);
+PrintMatrix(matrix);
+ReverseArray(matrix);
+PrintMatrix(matrix);
+
+
+
+// void ReverseArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length / 2; i++)
+//     {
+//         int temp = array[i];
+//         array[i] = array[array.Length - 1 - i];
+//         array[array.Length - 1 - i] = temp;
+//     }
+// }
---
+++ seminar8/tast55/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+   
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+            //Console.Write($"{matrix[i, j] } ");
+        }
+        //Console.WriteLine();
+    }
+    return matrix;
+
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ReversMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            Console.Write($"{matrix[j, i]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+int[,] matr = GetMatrix(m,n);
+PrintMatrix(matr);
+Console.WriteLine();
+ReversMatrix(matr);
+
+
+
+// Console.WriteLine();
+// ReversMatrix(matrix);
