035e1462fec1b7bd2eaadc2efc568a0e7a55b2f1
Sultanov Rustam <v.p.town@gmail.com>
2022-10-14 20:11:38 +0500

    hw62

--- seminar8/hw62/Program.cs
+++ seminar8/hw62/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 62. Заполните спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+
+
+int len = 4;
+int[,] table = new int[len, len];
+FillmatrixSpiral(table, len);
+Printmatrix(table);
+
+void FillmatrixSpiral(int[,] matrix, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { matrix[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) matrix[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) matrix[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) matrix[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+void Printmatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < 10)
+            {
+                Console.Write("0" + matrix[i, j]);
+                Console.Write(" ");
+            }
+            else Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
