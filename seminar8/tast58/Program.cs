8ad5c0c54f05a9f362cbc87c1e2cf2d4fbe777c5
Sultanov Rustam <v.p.town@gmail.com>
2022-10-14 19:22:57 +0500

    hw58

---
+++ seminar8/tast58/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, заданы 2 массива:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// и
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+// Их произведение будет равно следующему массиву:
+// 1 20 56 10
+// 20 81 8 6
+// 56 8 4 24
+// 10 6 24 49
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+
+int[,] matrix = new int[m, n];
+int[,] secondMatrix = new int[n, m];
+int[,] resultMatrix = new int[n, m];
+GetMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+GetMatrix(secondMatrix);
+PrintMatrix(secondMatrix);
+Console.WriteLine();
+
+
+if (matrix.GetLength(0) != secondMatrix.GetLength(1))
+{
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < resultMatrix.GetLength(0); i++)
+{
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+        int sum = 0;
+
+        sum = matrix[i, j] * secondMatrix[i, j];
+
+        resultMatrix[i, j] = sum;
+    }
+}
+
+PrintMatrix(resultMatrix);
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
